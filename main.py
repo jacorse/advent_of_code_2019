56edda224d3e8ab6816ff7fbfc3cec4f45f89032
James Corse <jacorse@wpi.edu>
2019-12-05 14:01:33 -0800

    day 1 part 1

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 # Advent of Code 2019
 
+
 def main():
-	#coding is fun
+    # coding is fun
+    filename = "aoc_input1.txt"
+
+    fp = open(filename)
+
+    inputs = fp.readlines()
+    print(len(inputs))
+
+    total = 0
+
+    for mass in inputs:
+        num = int(mass.strip())
+        num = int(num/3)
+        num -= 2
+        total += num
+
+    print(total)
+
+    fp.close()
+
+
+main()
